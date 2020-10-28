ec3427b6739f3ede3ef9739f3682e1e1b3bba94a
Mattias Torstensson <mattiastorstensson91@gmail.com>
2020-10-28 21:42:48 +0100

    router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react"
 import About from "./About"
 import Contact from "./Contact"
 import Username from "./Username"
+import {Link} from "react-router-dom"
 
 function App() {
   return (
-    <Username.Provider value="Mattias">
+    <Username.Provider value="Kalle">
+      <Link to="/">Home</Link>
+      <Link to="/blog">Blog</Link>
       <About />
       <Contact />
     </Username.Provider>
