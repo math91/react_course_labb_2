36488d5add0ef5606233d6c21aa2d7adc136fc22
Mattias Torstensson <mattiastorstensson91@gmail.com>
2020-10-29 18:39:45 +0100

    app update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,43 @@ import React from "react"
 import About from "./About"
 import Contact from "./Contact"
 import Username from "./Username"
-import {Link} from "react-router-dom"
+import {Link, Switch, Route} from "react-router-dom"
+import styled from "styled-components"
+
 
 function App() {
+
+  const Button = styled.button`
+    color: palevioletred;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+    cursor: pointer;
+
+    &:hover {
+      border: 2px solid Violet;
+    }
+  `;
+
   return (
     <Username.Provider value="Kalle">
       <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
-      <About />
-      <Contact />
+      <Link to="/contact">Kontakta</Link>
+
+      <Button>Click me</Button>
+
+      <Switch>
+        <Route exact path="/">
+          <About />
+        </Route>
+
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+
     </Username.Provider>
   )
 }
